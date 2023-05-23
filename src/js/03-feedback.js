747fd599107cac1b2245d38e25a8980e6387bb0c
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-05-23 21:37:25 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const formState = {
+  email: '',
+  message: '',
+};
+
+(() => {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (!savedState) {
+    return;
+  }
+
+  const { email, message } = JSON.parse(savedState);
+
+  emailInput.value = email;
+  messageInput.value = message;
+})();
+
+const saveFormState = throttle(() => {
+  formState.email = emailInput.value;
+  formState.message = messageInput.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+form.addEventListener('input', saveFormState);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  console.log('Form submitted with values:', {
+    email: formState.email,
+    message: formState.message,
+  });
+
+  localStorage.removeItem('feedback-form-state');
+
+  form.reset();
+
+  formState.email = '';
+  formState.message = '';
+});
