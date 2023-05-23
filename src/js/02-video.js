be2eafb625c726673e6f6bac35e40176098aa8fb
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-05-23 21:36:30 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function ({ seconds }) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY || 0));
